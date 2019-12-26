63ad1cfd28f2aa6ea152aed9f180277bb9908429
Pavel Blinov <blinovps@gmail.com>
2019-12-26 15:10:09 +0300

    Первый урок

--- Lesson01/1.3.py
+++
@@ -1,4 +0,0 @@
-num = input("Введите число:")
-num1 = int(num + num)
-num2 = int(num + num + num)
-print(int(num) + num1 + num2)
--- Lesson01/1.6.py
+++
@@ -1,7 +0,0 @@
-start = int(input("Введите начальный результат:"))
-finish = int(input("Введите конечный результат:"))
-day = 1
-while finish > start:
-    start += start * 0.1
-    day += 1
-print(f"Результат будет достигнут на {day} день")
